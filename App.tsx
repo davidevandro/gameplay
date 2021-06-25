2e2361bba2aea01b8e1a9e946e2545f8d8545144
David Martins <davidevandro@gmail.com>
2021-06-24 22:49:53 -0300

    Utilizando fontes personalizadas e fazendo melhorias na tela de SignIn

--- App.tsx
+++ App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'react-native';
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
+
 import { SignIn } from './src/screens/SignIn';
+import AppLoading from 'expo-app-loading';
+
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium, 
+    Rajdhani_700Bold
+  });
+
+  if (!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
-    <SignIn/>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    
+      <SignIn/>
+    </>
   );
 }
