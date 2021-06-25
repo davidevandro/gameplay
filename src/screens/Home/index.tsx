681aeab89dd251dddab32ea7832d2db75f01031e
David Martins <davidevandro@gmail.com>
2021-06-25 19:12:06 -0300

    Barra de categorias com scroll e check/uncheck

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View } from 'react-native'; 
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
@@ -7,6 +7,12 @@ import { Profile } from '../../components/Profile';
 import { styles } from './styles';
 
 export function Home(){
+    const [category, setCategory] = useState('');
+
+    function handleCategorySelect(categoryId: string){
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
+
     return(
         <View>
             <View style = {styles.header}>
@@ -14,7 +20,10 @@ export function Home(){
                 <ButtonAdd/>
             </View>
             <View>
-                <CategorySelect/>
+                <CategorySelect
+                    categorySelected={category}
+                    setCategory={handleCategorySelect}
+                />
             </View>
         </View>
     );
