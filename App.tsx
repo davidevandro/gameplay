5aac351756783c7ec2b60d216caa1e6e9452f239
David Martins <davidevandro@gmail.com>
2021-06-25 09:16:19 -0300

    Barra de Profile

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'react-native';
 import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
 
-import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 import AppLoading from 'expo-app-loading';
 import {Background} from './src/components/Background'
 
@@ -29,7 +29,7 @@ export default function App() {
         translucent
       />
     
-      <SignIn/>
+      <Home/>
     </Background>
   );
 }
