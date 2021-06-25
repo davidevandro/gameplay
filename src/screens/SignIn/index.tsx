59cd44021b543dd33b9ab7fab0a9beb62fdaffa0
David Martins <davidevandro@gmail.com>
2021-06-25 11:54:43 -0300

    Navegacao entre SignIn e Home

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -8,8 +8,16 @@ import {
 import  { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 
 export function SignIn(){
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
     return(
         <View style = {styles.container}>
             <Image 
@@ -32,6 +40,7 @@ export function SignIn(){
                 <ButtonIcon 
                     title="Entrar com Discord"
                     activeOpacity={0.7}
+                    onPress = {handleSignIn}
                 />
             </View>
         </View>
